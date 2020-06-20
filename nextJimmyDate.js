0bdf06f076a4de6b27727b3c1770eebf5d7d7476
Treescrub <treescrub@gmail.com>
2020-06-20 16:50:00 -0600

    Added basic text and updating script

---
+++ nextJimmyDate.js
@@ -0,0 +1,46 @@
+function getDayOfYear(){
+	var now = new Date()
+	var start = new Date(now.getFullYear(), 0, 0)
+	var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000)
+	var oneDay = 1000 * 60 * 60 * 24
+	var day = Math.floor(diff / oneDay) - 1
+	
+	return day
+}
+
+function jimmyCanSpawn(){
+	var now = new Date()
+	var year = now.getFullYear()
+	var day = getDayOfYear()
+	
+	return (year > 2009 || day > 330) && (year * 365 - 733616 + day) % 15 == 0
+}
+
+function getNextJimmyDate(){
+	var now = new Date()
+	var year = now.getFullYear()
+	var day = getDayOfYear()
+	
+	if(jimmyCanSpawn()){
+		day++
+	}
+	
+	return new Date(new Date(year, 0).setDate(day + (15 - ((year * 365 - 733616 + day) % 15)) + 1))
+}
+
+var updateInterval = 15 * 1000
+
+var updateCount = 0
+
+function Update(){
+	setTimeout(Update, updateInterval)
+	
+	document.getElementById("next-spawn-time").textContent = getNextJimmyDate()
+	document.getElementById("can-spawn").textContent = jimmyCanSpawn()
+	document.getElementById("update-count").textContent = updateCount
+	
+	updateCount++
+	//window.alert("okay")
+}
+
+Update()
