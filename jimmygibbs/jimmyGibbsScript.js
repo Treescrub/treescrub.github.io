64a00d693efc697e3756b6c58d36e594986394ce
Treescrub <treescrub@gmail.com>
2020-07-05 12:29:25 -0600

    Added previous spawn time, improved grammar

--- jimmygibbs/jimmyGibbsScript.js
+++ jimmygibbs/jimmyGibbsScript.js
@@ -16,6 +16,21 @@ function jimmyCanSpawn(){
 	return (year > 2009 || day > 330) && (year * 365 - 733616 + day) % 15 == 0
 }
 
+function getLastJimmyDate(){
+	var now = new Date()
+	var year = now.getFullYear()
+	var day = getDayOfYear()
+	
+	if(jimmyCanSpawn()){
+		day--
+	}
+	
+	var date = new Date(year, 0)
+	date.setDate(day - ((year * 365 - 733616 + day) % 15) + 1)
+	
+	return date
+}
+
 function getNextJimmyDate(){
 	var now = new Date()
 	var year = now.getFullYear()
@@ -25,7 +40,10 @@ function getNextJimmyDate(){
 		day++
 	}
 	
-	return new Date(new Date(year, 0).setDate(day + (15 - ((year * 365 - 733616 + day) % 15)) + 1))
+	var date = new Date(year, 0)
+	date.setDate(day + (15 - ((year * 365 - 733616 + day) % 15)) + 1)
+	
+	return date
 }
 
 var updateInterval = 60 * 1000
@@ -36,8 +54,10 @@ function Update(){
 	setTimeout(Update, updateInterval)
 	
 	var nextDate = getNextJimmyDate()
+	var lastDate = getLastJimmyDate()
 	//document.getElementById("next-spawn-time-text").textContent = ""
 	document.getElementById("next-spawn-time").textContent = nextDate.toLocaleString("default", {month: 'long'}) + ' ' + nextDate.getDate() + ' ' + nextDate.getFullYear()
+	document.getElementById("last-spawn-time").textContent = lastDate.toLocaleString("default", {month: 'long'}) + ' ' + lastDate.getDate() + ' ' + lastDate.getFullYear()
 	document.getElementById("can-spawn").textContent = jimmyCanSpawn() ? "Jimmy Gibbs Jr. can currently spawn" : "Jimmy Gibbs Jr. cannot currently spawn"
 	//document.getElementById("update-count").textContent = updateCount
 	
